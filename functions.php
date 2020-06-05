33419a3807eae0f615619bbb3c7dbbbf20e770e1
adgjmptwgw <adgjmptwgw@gmail.com>
2020-06-05 15:43:52 +0900

    ここにコメントを書く

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+//関数を定義
+function connect_to_db()  
+{
+  $dbn = 'mysql:dbname=gsacf_l03_03;charset=utf8;port=3306;host=localhost';
+  $user = 'root';
+  $pwd = '';
+
+  try {
+    // ここでDB接続処理を実行する
+    return new PDO($dbn, $user, $pwd);
+  } catch (PDOException $e) {
+    // DB接続に失敗した場合はここでエラーを出力し，以降の処理を中止する
+    // echo json_encode(["db error" => "{$e->getMessage()}"]);
+    // exit();
+    exit('dbError:' . $e->getMessage());
+  }
+
+}
